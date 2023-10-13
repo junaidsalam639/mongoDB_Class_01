eea31c20de0729f103115b17b8e4b5136a859dcf
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-13 22:09:17 +0500

    'mongoose_mongoDB'

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const userModel = require('./Model/user');
 const blogRouter = require('./Route/blog');
 const userRouter = require('./Route/user')
 
