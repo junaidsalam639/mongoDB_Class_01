98ad617b673d75084e2a1e9f970a78b5a604a1a9
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-14 21:52:46 +0500

    'mongoDB_moongoose'

--- Model/student.js
+++ Model/student.js
@@ -4,10 +4,17 @@ const studentSchema = new mongoose.Schema({
     studentName : {type : mongoose.SchemaTypes.String , required : true},
     studentClass : {type : mongoose.SchemaTypes.String , required : true},
     studentSubject : {type : mongoose.SchemaTypes.String , required : true},
-});
+},{timestamps : true});
 
 const student = mongoose.model('students' , studentSchema);
 
 module.exports = student
 
 
+
+// {
+//     "studentName" : "mongoDB And mongoose",
+//       "studentClass" : "Web And App Development",
+//       "studentSubject" : "Javascript" 
+//   }
+  
