968ded3bea1b5578e6bbb078c481b291f97b6857
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-18 22:08:16 +0500

    'blog__'

--- Model/user.js
+++ Model/user.js
@@ -4,7 +4,6 @@ const userScheme = new mongoose.Schema ({
     username : {type : mongoose.SchemaTypes.String, required : true },
     email : {type : mongoose.SchemaTypes.String, required : true , unique : true},
     password : {type : mongoose.SchemaTypes.String, required : true},
-    student : {type : mongoose.SchemaTypes.ObjectId , ref : 'students' , required : true}
 }, {timestamps : true})
 
 const user = mongoose.model('users' , userScheme);
