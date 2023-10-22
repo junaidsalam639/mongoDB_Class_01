d1b690d352e31d0e03682462c405030f7c9f17d3
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-22 23:19:21 +0500

    'practice_express'

--- Route/practice.js
+++ Route/practice.js
@@ -5,8 +5,28 @@ const practiceModel = require('../Model/practice');
 
 app.get('/' , async (req , res) => {
     const practice = await practiceModel.find();
-    
+    res.send({
+        status : 200,
+        practice
+    })
 })
+
+app.post('/' , async (req , res) => {
+   const practice = await practiceModel.create({...req.body});
+   res.send({
+    status : 200,
+    practice
+   })
+})
+
+app.put('/:id' , async (req , res) => {
+    const practice = await practiceModel.findByIdAndUpdate(req.params.id , {...req.body});
+    res.send({
+        status : 200,
+        practice
+    })    
+})
+
 
 module.exports = app
 
