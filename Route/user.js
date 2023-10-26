97ea136d3bdf7f416a1578081f8f017ef65179f2
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-26 21:02:50 +0500

    'bcrypt'

--- Route/user.js
+++ Route/user.js
@@ -30,16 +30,16 @@ app.get('/:id', async (req, res) => {
 
 app.post('/', async (req, res) => {
     try {
-        console.log('Password===>' , req.body.password);
+        console.log('Password===>', req.body.password);
         // Generate Hashed Password
         const saltRounds = 10;
         const salt = await bcrypt.genSaltSync(saltRounds)
-        const hash = await bcrypt.hashSync(req.body.password , salt);
+        const hash = await bcrypt.hashSync(req.body.password, salt);
         req.body.password = hash  // Body password Hashed Update 
-        
+
         const user = await userModel.create({ ...req.body });
-        console.log('Hashed Password===>' , hash);
-        user.password = undefined
+        console.log('Hashed Password===>', hash);
+        user.password = undefined  // User ka Password Response me Nhi Dikhae
         res.send({
             status: 200,
             user
@@ -49,6 +49,41 @@ app.post('/', async (req, res) => {
     }
 });
 
+
+app.post('/login', async (req , res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email: email });
+        console.log('user===>',user);
+        if (user) {
+            const isPasswordValid = bcrypt.compareSync(password, user.password);
+            user.password = undefined
+            if (isPasswordValid) {
+                res.status(200).send({
+                    status: 200,
+                    msg: 'User Login',
+                    user
+                })
+            } else {
+                res.status(401).send({
+                    status: 401,
+                    error: true,
+                    msg: 'Password is Not Valid',
+                })
+            }
+        }
+        else {
+            res.status(401).send({
+                status: 401,
+                error: true,
+                msg: 'Email Does Not Exists',
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        console.log('User Not Found');
+    }
+});
 
 
 app.delete('/:id', async (req, res) => {
