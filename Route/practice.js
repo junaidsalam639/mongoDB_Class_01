49920362f1a150bca012a4ed99a2a9bb3c8d7c28
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-29 12:06:28 +0500

    'jwt_web_token'

--- Route/practice.js
+++ Route/practice.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const practiceSchema = new mongoose.Schema({
     username : {type : mongoose.SchemaTypes.String , required : true},
@@ -20,6 +22,33 @@ app.get('/practice' , async (req , res) => {
         practice
     })
 })
+
+app.post('/practice' , async (req , res) => {
+    const rounds = 10
+    const saltRounds = await bcrypt.genSaltSync(rounds);
+    const hash = await bcrypt.hashSync(saltRounds);
+    req.body.password = hash
+
+    const practice = await practiceModel.create({...req.body});
+    req.body.password = undefined
+    
+    const token = jwt.sign({
+        data : practice
+    } , 'ajhfkjfgdhdbfvvsdfgkjauirgfdghfdgsjsfdgsds')
+    console.log(token);
+
+    res.status(200).send({
+        status : 200,
+        practice
+    })
+})
+
+
+
+
+
+
+
 
 
 
