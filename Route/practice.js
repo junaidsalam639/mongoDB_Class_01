f8b9ff1db4d3a0cc6ae421eab773904721024133
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-29 12:16:07 +0500

    'jwtwebtoken_bcrypt'

--- Route/practice.js
+++ Route/practice.js
@@ -5,40 +5,35 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const practiceSchema = new mongoose.Schema({
-    username : {type : mongoose.SchemaTypes.String , required : true},
-    email : {type : mongoose.SchemaTypes.String , required : true , unique : true},
-    password : {type : mongoose.SchemaTypes.String , required : true},
-    blog : {type : mongoose.SchemaTypes.ObjectId , ref : 'blogs' , required : true},
+    username: { type: mongoose.SchemaTypes.String, required: true },
+    email: { type: mongoose.SchemaTypes.String, required: true, unique: true },
+    password: { type: mongoose.SchemaTypes.String, required: true },
+    blog: { type: mongoose.SchemaTypes.ObjectId, ref: 'blogs', required: true },
 })
 
-const practiceModel = mongoose.model('practices' , practiceSchema);
+const practiceModel = mongoose.model('practices', practiceSchema);
 
 
 
-app.get('/practice' , async (req , res) => {
+app.get('/practice', async (req, res) => {
     const practice = await practiceModel.find().populate('blog').exec();
     res.status(200).send({
-        status : 200,
+        status: 200,
         practice
     })
 })
 
-app.post('/practice' , async (req , res) => {
+app.post('/practice', async (req, res) => {
     const rounds = 10
     const saltRounds = await bcrypt.genSaltSync(rounds);
     const hash = await bcrypt.hashSync(saltRounds);
     req.body.password = hash
 
-    const practice = await practiceModel.create({...req.body});
+    const practice = await practiceModel.create({ ...req.body });
     req.body.password = undefined
-    
-    const token = jwt.sign({
-        data : practice
-    } , 'ajhfkjfgdhdbfvvsdfgkjauirgfdghfdgsjsfdgsds')
-    console.log(token);
 
     res.status(200).send({
-        status : 200,
+        status: 200,
         practice
     })
 })
@@ -47,7 +42,40 @@ app.post('/practice' , async (req , res) => {
 
 
 
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body
+    const practice = await practiceModel.findOne({ email: email });
 
+    if (practice) {
+        const isPassword = await bcrypt.compareSync(password, practice.password);
+        console.log(isPassword);
+        practice.password = undefined
+
+        const token = jwt.sign({
+            data: practice
+        }, 'ajhfkjfgdhdbfvvsdfgkjauirgfdghfdgsjsfdgsds')
+        console.log(token);
+
+        if (isPassword) {
+            res.status(200).send({
+                status : 200,
+                token,
+                practice
+            })
+        }
+        else{
+            res.status(401).send({
+                status : 401,
+                msg : "Password does Not Exists"
+            })
+        }
+    }else{
+        res.status(401).send({
+            status : 401,
+            msg : "Email does Not Exists"
+        })
+    }
+})
 
 
 
