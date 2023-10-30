00b555f2d92d29c441fab7012a8f98b8fda4e085
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-30 20:06:16 +0500

    'json_web_token_hash'

--- Route/practice.js
+++ Route/practice.js
@@ -28,73 +28,91 @@ route.get('/', async (req, res) => {
 
 // User Password Hash Me Convert
 route.post('/', async (req, res) => {
-    console.log(req.body.password);
-    const round = 10;
-    const salt = await bcrypt.genSaltSync(round);
-    const hash = await bcrypt.hashSync(req.body.password, salt);
-    req.body.password = hash
-    console.log(hash);
-    const practice = await model.create({ ...req.body });
-    practice.password = undefined
-    res.status(200).send({
-        status: 200,
-        practice
-    })
+    try {
+        console.log(req.body.password);
+        const round = 10;
+        const salt = await bcrypt.genSaltSync(round);
+        const hash = await bcrypt.hashSync(req.body.password, salt);
+        req.body.password = hash
+        console.log(hash);
+        const practice = await model.create({ ...req.body });
+        practice.password = undefined
+        res.status(200).send({
+            status: 200,
+            practice
+        })
+    } catch (err) {
+        res.status(403).send({
+            status: 403,
+            masg: 'crash Does Not Exists',
+            error: true
+        })
+    }
 })
 
 
 // User Find And Password Compare And User Token Generate
 route.post('/login', async (req, res) => {
-    const { email, password } = req.body
-    const isUser = await model.findOne({ email: email });
-    console.log(isUser);
-    if (isUser) {
-        const isPassword = await bcrypt.compareSync(isUser.password, password);
-        console.log(isPassword);
-        isUser.password = undefined
-        const token = jwt.sign({
-            data: isUser
-        }, 'dkjsfjhsdfgsdfhjfgsdhhfsdfsg')
-        if (isPassword) {
-            res.status(200).send({
-                status : 200,
-                token ,
-                isUser
-            })
-        }else{
+    try {
+
+        const { email, password } = req.body
+        const isUser = await model.findOne({ email: email });
+        console.log(isUser);
+        if (isUser) {
+            const isPassword = await bcrypt.compareSync(isUser.password, password);
+            console.log(isPassword);
+            isUser.password = undefined
+            const token = jwt.sign({
+                data: isUser
+            }, 'dkjsfjhsdfgsdfhjfgsdhhfsdfsg')
+            if (isPassword) {
+                res.status(200).send({
+                    status: 200,
+                    token,
+                    isUser
+                })
+            } else {
+                res.status(403).send({
+                    status: 403,
+                    masg: 'Password Does Not Exists',
+                    error: true
+                })
+            }
+        } else {
             res.status(403).send({
-                status : 403,
-                masg : 'Password Does Not Exists',
-                error : true
+                status: 403,
+                masg: 'Email Does Not Exists',
+                error: true
             })
         }
-    }else{
+    } catch (err) {
         res.status(403).send({
-            status : 403,
-            masg : 'Email Does Not Exists',
-            error : true
+            status: 403,
+            masg: 'Email Does Not Exists',
+            error: true
         })
     }
+
 })
 
 
 
 
 // User MiddleWare Function WithOut Token Not Entry
-const authentication = async (req , res , next) => {
+const authentication = async (req, res, next) => {
     console.log(req.headers);
     const token = req.headers?.authorization?.split(' ')[1];
     console.log(token);
-    if(token){
-       const verify = await jwt.verify(token , 'dkjsfjhsdfgsdfhjfgsdhhfsdfsg');
-       console.log(verify);
-       next();
+    if (token) {
+        const verify = await jwt.verify(token, 'dkjsfjhsdfgsdfhjfgsdhhfsdfsg');
+        console.log(verify);
+        next();
     }
-    else{
+    else {
         res.status(403).send({
-            status : 403,
-            masg : 'Toekn Does Not Exists',
-            error : true
+            status: 403,
+            masg: 'Toekn Does Not Exists',
+            error: true
         })
     }
 }
